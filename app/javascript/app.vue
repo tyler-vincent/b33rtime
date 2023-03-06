66896746ffa080966983a891447d212a9163956d
Tyler Vincent <tyler@tindaleholt.com>
2023-03-06 12:17:57 -0500

    Starting on styling, splitting out into more components

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,47 +1,34 @@
 <template>
   <div>
-    <h1 class="text-center">Upload</h1>
+    <header class="sticky top-0 flex w-full items-center justify-between py-6 bg-white z-50">
+      b33rti.me
+      <upload-image></upload-image>
+    </header>
+    <main class=relative>
+      <h1 class="text-center">Images</h1>
 
-    <upload-image></upload-image>
+      <div v-masonry="containerId" transition-duration="0.3s" item-selector=".upload" gutter="0" fit-width="true" class="uploads-container" >
+        <div class="upload" v-for="(item, index) in uploads" :key="index" v-masonry-tile @mouseover="$redrawVueMasonry()" @mouseleave="$redrawVueMasonry()">
 
-    <h1 class="text-center">Images</h1>
+          <upload-card :thumbnail-url="item.thumbnail_url" :full-url="item.full_url"></upload-card>
 
-    <div v-masonry="containerId" transition-duration="0.3s" item-selector=".upload" gutter="0" fit-width="true" class="uploads-container" >
-      <div class="upload" v-for="(item, index) in uploads" :key="index" v-masonry-tile @mouseover="$redrawVueMasonry()" @mouseleave="$redrawVueMasonry()">
-        <img :src="item.thumbnail_url" class="thumbnail" />
+
+        </div>
       </div>
-    </div>
 
-    <infinite-loading target="#app" @infinite="getUploads"></infinite-loading>
-
-<div class="w-full max-w-sm rounded bg-white h-96 m-auto shadow flex flex-col">
-    <div class="mx-8 shadow rounded-full h-10 mt-4 flex p-1 relative items-center">
-        <div class="w-full flex justify-center">
-            <button>Left</button>
-        </div>
-        <div class="w-full flex justify-center">
-            <button>Right</button>
-        </div>
-        <span
-        class="elSwitch bg-indigo-600 shadow text-white flex items-center justify-center w-1/2 rounded-full h-8 transition-all top-[4px] absolute left-1 ">
-        Text
-        </span>
-    </div>
-</div>
-
-
+      <infinite-loading target="#app" @infinite="getUploads"></infinite-loading>
+    </main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
 import UploadImage from "~/components/UploadImage.vue";
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue';
-import 'virtual:windi.css';
-
+import UploadCard from "~/components/UploadCard.vue";
 
 export default {
   created: function() {
-    console.log('Component has been created!')
+    console.log('Main component has been created!')
   },
   mounted: function() {
     this.getUploads();
@@ -55,17 +42,14 @@ export default {
   },
   components: {
     UploadImage,
-    TabGroup,
-    TabList,
-    Tab,
-    TabPanels,
-    TabPanel
+    UploadCard
   },
   methods: {
     async getUploads () {
       console.log(`Loading Page ${this.page}`);
       const response = await this.$http.get(`/uploads.json?page=${this.page}`);
       this.uploads = this.uploads.concat(response.data);
+      console.log(this.uploads);
       this.page++;
     },
     setImage: function (resizedFile) {
@@ -80,20 +64,5 @@ export default {
   .uploads-container {
     display: block;
     margin: auto;
-  }
-
-  .upload {
-    width: 150px;
-    height: 150px;
-
-    .thumbnail {
-      width: 100%;
-      height: 100%;
-    }
-
-    &:hover {
-      width: 300px;
-      height: 300px;
-    }
   }
 </style>
--- app/javascript/components/UploadImage.vue
+++ app/javascript/components/UploadImage.vue
@@ -1,69 +1,132 @@
 
 <template>
   <div>
-    <div class="row">
-      <div class="col-8">
-        <label class="btn btn-default p-0">
-          <input
-            type="file"
-            accept="image/*"
-            ref="file"
-            @change="selectImage"
-          />
-        </label>
-      </div>
 
-      <div class="col-4">
-        <button
-          class="btn btn-success btn-sm float-right"
-          :disabled="!currentImage"
-          @click="upload"
-        >
-          Upload
-        </button>
-      </div>
-    </div>
-
-    <div v-if="currentImage" class="progress">
-      <div
-        class="progress-bar progress-bar-info"
-        role="progressbar"
-        :aria-valuenow="progress"
-        aria-valuemin="0"
-        aria-valuemax="100"
-        :style="{ width: progress + '%' }"
-      >
-        {{ progress }}%
-      </div>
-    </div>
-
-    <div v-if="previewImage">
-      <div>
-        <img class="preview my-3" :src="previewImage" alt="" />
-      </div>
-    </div>
 
     <div v-if="message" class="alert alert-secondary" role="alert">
       {{ message }}
     </div>
 
-    <div class="card mt-3">
-      <div class="card-header">List of Images</div>
-      <ul class="list-group list-group-flush">
-        <li
-          class="list-group-item"
-          v-for="(image, index) in imageInfos"
-          :key="index"
-        >
-          <a :href="image.url">{{ image.name }}</a>
-        </li>
-      </ul>
+
+    <div class="">
+      <button
+        type="button"
+        @click="openModal"
+        class="rounded-md bg-black px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        Upload
+      </button>
     </div>
+
+
+
+  <TransitionRoot appear :show="uploadModalOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-black p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-100"
+              >
+                Upload
+              </DialogTitle>
+
+              <div class="mt-2">
+                <div class="row">
+                  <div class="col-8">
+                    <label class="btn btn-default p-0">
+                      <input
+                        type="file"
+                        accept="image/*"
+                        ref="file"
+                        @change="selectImage"
+                      />
+                    </label>
+                  </div>
+                </div>
+
+                <div v-if="currentImage" class="progress">
+                  <div
+                    class="progress-bar progress-bar-info"
+                    role="progressbar"
+                    :aria-valuenow="progress"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    :style="{ width: progress + '%' }"
+                  >
+                    {{ progress }}%
+                  </div>
+                </div>
+
+                <div v-if="previewImage">
+                  <div>
+                    <img class="preview my-3" :src="previewImage" alt="" />
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  :disabled="!currentImage"
+                  @click="upload"
+                >
+                  Upload
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
+
+
+
+
   </div>
 </template>
 
 <script>
   import UploadService from "../services/UploadFilesService";
+  import { ref } from 'vue';
+  import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+  } from '@headlessui/vue';
+
+
   export default {
     name: "upload-image",
     data() {
@@ -73,7 +136,15 @@
         progress: 0,
         message: "",
         imageInfos: [],
+        uploadModalOpen: false,
       };
+    },
+    components: {
+      TransitionRoot,
+      TransitionChild,
+      Dialog,
+      DialogPanel,
+      DialogTitle
     },
     methods: {
       selectImage() {
@@ -100,6 +171,12 @@
             this.currentImage = undefined;
           });
       },
+      closeModal: function () {
+        this.uploadModalOpen = false
+      },
+      openModal: function () {
+        this.uploadModalOpen = true
+      }
     },
     mounted() {
       UploadService.getFiles().then((response) => {
@@ -110,7 +187,5 @@
 </script>
 
 <style scoped>
-.preview {
-  max-width: 200px;
-}
+
 </style>

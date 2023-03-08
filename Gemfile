5a8eb4fc8c12f6e16c3d3582a3dc433318f22a74
Tyler Vincent <tyler@tindaleholt.com>
2023-03-08 10:54:38 -0500

    Migrating to Postgres

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ ruby '3.2.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 7.0.4.2', '>= 5.2.4.1'
-# Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.4.4', '< 0.6.0'
+# Use postgres as the database for Active Record
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 gem 'jbuilder', '~> 2.5'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_07_163550) do
-  create_table "uploads", id: :integer, charset: "utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "uploads", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "md5_name"
     t.string "token"
     t.index ["token"], name: "index_uploads_on_token"
